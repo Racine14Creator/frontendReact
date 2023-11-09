41bb4dfab3c73d0a29b45674a3d0edc38f4d6b1c
Jean-Pierre14 <chiruzabisimwa@gmail.com>
2023-11-09 14:42:48 +0200

    add: ?! init project

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="shrink-0">
+        img
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">Title</div>
+        <p className="text-slate-500">message</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
